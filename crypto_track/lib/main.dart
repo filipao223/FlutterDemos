fe688d148f361716baf20416ec1b5efcd7e1c276
filipao223 <jpmontenegro67@gmail.com>
2019-11-15 23:16:05 +0000

    Pulls cryptocoin info from API and displays it in a list.

--- crypto_track/lib/main.dart
+++ crypto_track/lib/main.dart
@@ -1,6 +1,14 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:ffi';
 
-void main() => runApp(MyApp());
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:global_configuration/global_configuration.dart';
+import 'dart:developer';
+
+void main(){
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -20,92 +28,104 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: CryptoList()
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class CryptoListState extends State<CryptoList> {
+  final _cryptoList = List<String>();
+  String _apiKey = "null";
+  String _coinName = "null";
+  List<Coin> _coinList = List<Coin>();
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  void _getCryptoNames() async {
+    await GlobalConfiguration().loadFromPath("assets/config.json");
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+    _apiKey = GlobalConfiguration().get("api_key");
 
-  final String title;
+    // make GET request
+    String url = 'https://rest.coinapi.io/v1/assets?apikey=$_apiKey';
+    Response response = await get(url);
+    // sample info available in response
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+    int statusCode = response.statusCode;
+    Map<String, String> headers = response.headers;
+    String contentType = headers['content-type'];
+    String json = response.body;
+    // TODO convert json to object...
+    var dataMap = jsonDecode(json);
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      dataMap.forEach(
+              (data) => {
+                if (!data['name'].toString().contains("null") && (double.tryParse(data['price_usd'].toString()) != null)) {
+                  print(data["name"]),
+                  _coinList.add(new Coin(data['name'], data['asset_id'], double.parse(data['price_usd'].toString()), double.parse(data['volume_1mth_usd'].toString())))
+                }
+              }
+      );
+
+      /*Sort the list by volume in a month*/
+      _coinList.sort((coin1, coin2) => coin2.price.compareTo(coin1.price));
     });
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+  Widget build(BuildContext context){
+    if (_coinList.isEmpty){
+      return Scaffold(
+          appBar: AppBar(
+            title: Text("CryptoTrack"),
+          ),
+          body: Center(
+            child: Text("Getting info from API"),
+          )
+
+      );
+    }
+    else{
+      return Scaffold(
+          appBar: AppBar(
+            title: Text("CryptoTrack"),
+          ),
+          body: _buildList()
+
+      );
+    }
+  }
+
+  Widget _buildList(){
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i){
+        return _buildRow(_coinList[i]);
+      }
     );
   }
+
+  Widget _buildRow(Coin coin){
+    return ListTile(
+      title: Text("${coin.name} - Price: ${coin.price.toStringAsFixed(2)} USD"),
+    );
+  }
+
+}
+
+class CryptoList extends StatefulWidget{
+  @override
+  CryptoListState createState(){
+    CryptoListState cryptoListState = new CryptoListState();
+    cryptoListState._getCryptoNames();
+    return cryptoListState;
+  }
+}
+
+
+class Coin{
+  final String name;
+  final String id;
+  final double price;
+  final double volume_month;
+
+  Coin(this.name, this.id, this.price, this.volume_month);
 }

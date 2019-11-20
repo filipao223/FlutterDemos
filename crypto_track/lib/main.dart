ec46ddb87d0dc024a720c56614a1154d378f947c
filipao223 <jpmontenegro67@gmail.com>
2019-11-20 15:30:31 +0000

    Fixed routes.

--- crypto_track/lib/main.dart
+++ crypto_track/lib/main.dart
@@ -1,12 +1,21 @@
+
+
+
+
+import 'package:crypto_track/CryptoList.dart';
+import 'package:crypto_track/Favorites.dart';
 import 'package:flutter/material.dart';
-import 'package:crypto_track/Router.dart';
 import 'package:crypto_track/constants.dart';
 
 void main(){
   runApp(
     new MaterialApp(
-      onGenerateRoute: Router.generateRoute,
+      //onGenerateRoute: Router.generateRoute,
       initialRoute: homeRoute,
+      routes: {
+        homeRoute : (context) => CryptoList(),
+        favoritesRoute : (context) => Favorites()
+      },
     )
   );
 }

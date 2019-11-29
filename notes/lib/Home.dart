55586fbcc44687b1c7e73dedd5dd44cd319eb6d4
filipao223 <jpmontenegro67@gmail.com>
2019-11-29 23:51:47 +0000

    Added note list and card layout.

---
+++ notes/lib/Home.dart
@@ -0,0 +1,128 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class HomeState extends State<Home>{
+  
+  List<Note> noteList;
+
+  void createPlaceholderNotes(){
+    noteList = new List<Note>();
+    
+    noteList.add(new Note(noteId: 1, noteTitle: "title1", content: "test", dateCreated: DateTime.now(), dateLastEdited: DateTime.now()));
+    noteList.add(new Note(noteId: 2, noteTitle: "title2", content: "test", dateCreated: DateTime.now(), dateLastEdited: DateTime.now()));
+    noteList.add(new Note(noteId: 3, noteTitle: "title3", content: "test", dateCreated: DateTime.now(), dateLastEdited: DateTime.now()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Notes"),
+      ),
+      body: buildList(),
+    );
+  }
+
+  Widget buildList(){
+
+    return ListView.builder(
+      itemCount: noteList.length,
+      itemBuilder: (context, i){
+        return buildItem(noteList[i]);
+      },
+    );
+  }
+
+  Widget buildItem(Note note){
+
+    return Card(
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(left: 8.0),
+            child: Icon(Icons.insert_drive_file),
+          ),
+
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "${note.noteTitle}",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                          fontSize: 12
+                      ),
+                    ),
+                  ),
+
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "${note.content.substring(0, note.content.length>20 ? 20 : note.content.length)}",
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 10
+                      ),
+                    ),
+                  ),
+
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "${note.dateCreated.toIso8601String()}",
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 9
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.only(right: 8.0),
+            child: Icon(Icons.menu)
+          )
+        ],
+      ),
+    );
+  }
+
+}
+
+
+
+class Home extends StatefulWidget{
+
+  HomeState createState(){
+    HomeState homeState = new HomeState();
+    homeState.createPlaceholderNotes();
+    return homeState;
+  }
+}
+
+
+
+class Note{
+  final int noteId;
+  String noteTitle;
+  String content;
+  final DateTime dateCreated;
+  DateTime dateLastEdited;
+
+  Note({this.noteId, this.noteTitle, this.content, this.dateCreated, this.dateLastEdited});
+}
--- notes/lib/main.dart
+++ notes/lib/main.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:notes/constants.dart';
+import 'package:notes/Home.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-
-    );
-  }
+void main(){
+  runApp(
+      new MaterialApp(
+          theme: ThemeData(
+            brightness: Brightness.light,
+            primaryColor: Colors.blue,
+          ),
+          darkTheme: ThemeData(
+            primaryColor: Colors.grey,
+            brightness: Brightness.dark,
+          ),
+          //onGenerateRoute: Router.generateRoute,
+          initialRoute: homeRoute,
+          routes: {
+            homeRoute : (context) => Home(),
+          }
+      )
+  );
 }
+
